9d661b19325131d22c35e077eb19f6c864aa2caa
RookieNoob <lizhurui20040305@outlook.com>
2025-02-21 13:11:22 +0800

    保护数据库连接时用到的隐私信息

--- README.md
+++ README.md
@@ -21,6 +21,16 @@ A Flask based intelligent rental web application
 
 既然是“智能租房”项目，那么显然需要房源数据，而为了方便，本项目使用的示例数据来源于网络。数据内容可以查看`database`文件夹下的`house.sql`文件，其中包含建库语句与许多的插入语句
 
-首先在`MySQL`建立一个新的数据库，然后导入`house.sql`脚本，注意在执行脚本之前修改第一行的数据库名称为你自己建立的数据库名称，我这里的名字叫`easyhouse`
+首先在`MySQL`建立一个新的数据库，然后导入`house.sql`脚本，注意在执行脚本之前修改脚本中第一行的数据库名称为你自己建立的数据库名称，我这里的名字叫`easyhouse`
 
 当脚本中的内容全部执行完毕之后，查看数据表中的内容，如果数据无误则数据准备完成
+
+### 隐私保护
+
+在连接数据库的代码中你会看到其中的URI中包含了你的数据库的用户名和密码，这显然不应该直接写在代码里，因此这里我借助`dotenv`以加载环境变量的方式对其进行保护，你可以在项目的根目录下新建`.env`文件并在其中写入你自己的数据库账户信息：
+
+```python
+DB_USERNAME=YOUR_USERNAME
+DB_PASSWORD=YOUR_PASSWORD
+DB_DATABASE=YOUR_DATABASE
+```
--- settings.py
+++ settings.py
@@ -1,10 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+from dotenv import load_dotenv
+import os
 
 pymysql.install_as_MySQLdb()  # 以pymsql为MySQLdb的代理
 
-db = SQLAlchemy()
+load_dotenv()  # 从.env文件中加载环境变量
+
+db = SQLAlchemy()  # 实例化SQLAlchemy
+
 class Config:
     DEBUG = True  # 调试模式
-    SQLALCHEMY_DATABASE_URI = 'mysql://<用户名>:<密码>@<主机>:<端口>/<数据库名>'  # 数据库连接
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_DATABASE = os.getenv('DB_DATABASE')
+    SQLALCHEMY_DATABASE_URI = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:3306/{DB_DATABASE}'  # 数据库连接
     SQLALCHEMY_TRACK_MODIFICATIONS = True  # 是否追踪数据库的修改
